51c48ceeec8f36b74b86a1f74fdc83d9ffa1e808
Luan Ferreira <111436664+Luan-Ferreira-Dev@users.noreply.github.com>
2024-05-17 20:31:00 -0300

    :sparkles: feat: copy_subtitle.js

---
+++ Windows/copy_subtitle.js
@@ -0,0 +1,9 @@
+function copy_subtitle() {
+        var sub = mp.get_property("sub-text");
+        if (sub) {
+            sub = "'" + sub.split("'").join("''") + "'";
+            mp.commandv("run", "powershell", "Set-Clipboard", sub);
+            mp.osd_message("Copied to Clipboard");
+        }
+}
+mp.add_key_binding("Ctrl+c", copy_subtitle);
